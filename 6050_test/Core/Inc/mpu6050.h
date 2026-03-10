b3162c42f388272000ae3b2aa477f1ed5ce2062d
houjingl <jerryhou2005@outlook.com>
2026-03-10 12:30:30 -0400

    init commit

---
+++ 6050_test/Core/Inc/mpu6050.h
@@ -0,0 +1,74 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: 2026年3月8日
+ *      Author: 18701
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+#define TIMEOUT                   1000
+#define MPU6050_DEVICE_ADDRESS    0xD0
+#define MPU6050_REG_WHO_AM_I      0x75
+#define MPU6050_REG_PWR_MGMT_1    0x6B
+#define MPU6050_REG_PWR_MGMT_2    0x6C
+#define MPU6050_REG_ACCEL_CONFIG  0x1C
+#define MPU6050_REG_ACCEL_XOUT_H  0x3B
+#define MPU6050_REG_GYRO_CONFIG   0x1B
+#define MPU6050_REG_GYRO_XOUT_H   0x43
+#define MPU6050_REG_TEMP_OUT_H    0x41
+#define MPU6050_REG_SMPLRT_DIV	  0x19
+#define MPU6050_REG_INT_EN 		  0x38
+
+#define MPU6050_ACCEL_RANGE_2G     0x00
+#define MPU6050_ACCEL_RANGE_4G     0x01
+#define MPU6050_ACCEL_RANGE_8G     0x02
+#define MPU6050_ACCEL_RANGE_16G    0x03
+
+#define MPU6050_GYRO_RANGE_250     0x00
+#define MPU6050_GYRO_RANGE_500     0x01
+#define MPU6050_GYRO_RANGE_1000    0x02
+#define MPU6050_GYRO_RANGE_2000    0x03
+
+#define ACCEL_SCALE_FACTOR 16384.0f
+#define GYRO_SCALE_FACTOR 131.0f
+
+typedef struct{
+	uint8_t ClkSel: 3;
+	uint8_t Temp_Dis: 1;
+	uint8_t Reserved: 1;
+	uint8_t Cycle: 1;
+	uint8_t Sleep: 1;
+	uint8_t Device_Reset: 1;
+}PowerManagementRegister_t;
+
+typedef struct{
+	uint8_t Reserved: 3;
+	uint8_t AFS_Sel: 2;
+	uint8_t ZA_ST: 1;
+	uint8_t YA_ST: 1;
+	uint8_t XA_ST: 1;
+}AccelConfigRegister_t;
+
+typedef struct{
+	uint8_t Reserved: 3;
+	uint8_t FS_Sel: 2;
+	uint8_t ZG_ST: 1;
+	uint8_t YG_ST: 1;
+	uint8_t XG_ST:1;
+}GyroConfigRegister_t;
+
+HAL_StatusTypeDef mpu6050_read_reg(I2C_HandleTypeDef* hi2c, uint8_t reg_addr, uint16_t data_size, uint8_t* data_buf);
+HAL_StatusTypeDef mpu6050_write_reg(I2C_HandleTypeDef* hi2c, uint8_t reg_addr, uint8_t value);
+HAL_StatusTypeDef mpu6050_init(I2C_HandleTypeDef* hi2c, uint8_t AFS_SEL, uint8_t FS_SEL);
+void mpu6050_getAccelValue(I2C_HandleTypeDef *hi2cx, int16_t *accelData);
+void mpu6050_getGyroValue(I2C_HandleTypeDef *hi2cx, int16_t *gyroData);
+void mpu6050_toFloat(float* accel, float* gyro, int16_t* accel_raw, int16_t* gyro_raw);
+
+
+
+#endif /* INC_MPU6050_H_ */
---
+++ 6050_test/Core/Src/mpu6050.c
@@ -0,0 +1,132 @@
+/*
+ * mpu6050.c
+ *
+ *  Created on: 2026年3月10日
+ *      Author: 18701
+ */
+
+#include "mpu6050.h"
+
+HAL_StatusTypeDef mpu6050_read_reg(I2C_HandleTypeDef* hi2c, uint8_t reg_addr, uint16_t data_size, uint8_t* data_buf)
+{
+	if (HAL_I2C_Mem_Read(hi2c, MPU6050_DEVICE_ADDRESS, reg_addr, 1, data_buf, data_size, TIMEOUT) != HAL_OK){
+		print_msg("read reg failed\r\n");
+		return HAL_ERROR;
+	}
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef mpu6050_write_reg(I2C_HandleTypeDef* hi2c, uint8_t reg_addr, uint8_t value)
+{
+	uint8_t payload[2];
+	payload[0] = reg_addr;
+	payload[1] = value;
+
+	if (HAL_I2C_Master_Transmit(hi2c, MPU6050_DEVICE_ADDRESS, payload, 2, TIMEOUT) != HAL_OK){
+		print_msg("write reg failed\r\n");
+		return HAL_ERROR;
+	}
+
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef mpu6050_init(I2C_HandleTypeDef* hi2c, uint8_t AFS_SEL, uint8_t FS_SEL)
+{
+	uint8_t msg[100];
+	if (HAL_I2C_IsDeviceReady(hi2c, MPU6050_DEVICE_ADDRESS, 1, TIMEOUT) != HAL_OK){
+		print_msg("device not ready\r\n");
+		return HAL_ERROR;
+	}
+
+	uint8_t temp_reg = 0;
+
+
+	if (mpu6050_read_reg(hi2c, MPU6050_REG_WHO_AM_I, 1, &temp_reg) != HAL_OK){
+		return HAL_ERROR;
+	}
+
+	if (temp_reg != 0x68){
+		sprintf(msg, "Wrong product id (0x%x)\r\n", temp_reg);
+		print_msg(msg);
+		return HAL_ERROR;
+	}
+	// Writing 0x00 to the pwr management reg 0x6b to wake up the mpu6050
+	PowerManagementRegister_t powerManagement={0};
+	powerManagement.ClkSel = 0;
+	powerManagement.Temp_Dis = 0;
+	powerManagement.Reserved = 0;
+	powerManagement.Cycle = 0;
+	powerManagement.Sleep = 0;
+	powerManagement.Device_Reset = 0;
+	temp_reg = *((uint8_t*)&powerManagement);
+	if (mpu6050_write_reg(hi2c, MPU6050_REG_PWR_MGMT_1, temp_reg) != HAL_OK){
+		return HAL_ERROR;
+	}
+	// init accel reg -> AFS sel indicates range
+	AccelConfigRegister_t accelConfig = {0};
+	accelConfig.Reserved = 0;
+	accelConfig.AFS_Sel = AFS_SEL;
+	accelConfig.ZA_ST = 0;
+	accelConfig.YA_ST = 0;
+	accelConfig.XA_ST = 0;
+	temp_reg = *((uint8_t*)&accelConfig);
+	if (mpu6050_write_reg(hi2c, MPU6050_REG_ACCEL_CONFIG, temp_reg) != HAL_OK){
+		return HAL_ERROR;
+	}
+	// init gyro reg -> fs_sel indicates gyro range
+	GyroConfigRegister_t gyroConfig = {0};
+	gyroConfig.Reserved = 0;
+	gyroConfig.FS_Sel = FS_SEL;
+	gyroConfig.ZG_ST = 0;
+	gyroConfig.YG_ST = 0;
+	gyroConfig.XG_ST = 0;
+	temp_reg = *((uint8_t*)&gyroConfig);
+	if (mpu6050_write_reg(hi2c, MPU6050_REG_GYRO_CONFIG, temp_reg) != HAL_OK){
+		return HAL_ERROR;
+	}
+
+	// setting the sampling frequency to 1k hz
+	if (mpu6050_write_reg(hi2c, MPU6050_REG_SMPLRT_DIV, 0x7) != HAL_OK){
+		return HAL_ERROR;
+	}
+
+	if (mpu6050_write_reg(hi2c, MPU6050_REG_INT_EN, 0x1) != HAL_OK){
+		return HAL_ERROR;
+	}
+
+
+	return HAL_OK;
+}
+
+void mpu6050_getAccelValue(I2C_HandleTypeDef *hi2cx, int16_t *accelData)
+{
+	uint8_t data[6]={0};
+	mpu6050_read_reg(hi2cx, MPU6050_REG_ACCEL_XOUT_H, 6, data);
+	accelData[0]=(int16_t)(data[0]<<8 | data[1]);
+	accelData[1]=(int16_t)(data[2]<<8 | data[3]);
+	accelData[2]=(int16_t)(data[4]<<8 | data[5]);
+}
+
+void mpu6050_getGyroValue(I2C_HandleTypeDef *hi2cx, int16_t *gyroData)
+{
+	uint8_t data[6]={0};
+	mpu6050_read_reg(hi2cx, MPU6050_REG_GYRO_XOUT_H, 6, data);
+	gyroData[0]=(int16_t)(data[0]<<8 | data[1]);
+	gyroData[1]=(int16_t)(data[2]<<8 | data[3]);
+	gyroData[2]=(int16_t)(data[4]<<8 | data[5]);
+}
+
+void mpu6050_toFloat(float* accel, float* gyro, int16_t* accel_raw, int16_t* gyro_raw)
+{
+    // Convert Accelerometer data (X, Y, Z)
+    accel[0] = accel_raw[0] / ACCEL_SCALE_FACTOR;
+    accel[1] = accel_raw[1] / ACCEL_SCALE_FACTOR;
+    accel[2] = accel_raw[2] / ACCEL_SCALE_FACTOR;
+
+    // Convert Gyroscope data (X, Y, Z)
+    gyro[0] = gyro_raw[0] / GYRO_SCALE_FACTOR;
+    gyro[1] = gyro_raw[1] / GYRO_SCALE_FACTOR;
+    gyro[2] = gyro_raw[2] / GYRO_SCALE_FACTOR;
+}
+
+
